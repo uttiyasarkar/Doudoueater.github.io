1c420000c4c7d313bedcd75a37c417f9489e9ad6
uttiya <uttiyasarkar@gmail.com>
2023-04-04 00:56:49 +0530

    first commit

---
+++ main.php
@@ -0,0 +1,14 @@
+<div id="power-meter">
+  <div id="meter-container">
+    <div id="meter-scale">
+      <div class="meter-tick"></div>
+      <div class="meter-tick"></div>
+      <!-- Add more meter-tick divs for additional ticks on the scale -->
+    </div>
+    <div id="meter-arrow"></div>
+  </div>
+  <div>
+    <button onclick="increaseValue()">Increase Power</button>
+    <button onclick="decreaseValue()">Decrease Power</button>
+  </div>
+</div>
